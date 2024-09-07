2004e7102428e67826252185a276d0b426145cbe
DanilChizhikov <clichindustries@gmail.com>
2024-09-07 22:57:17 +0700

    removed ad response

--- Assets/AdUnification/Runtime/Responses/AdResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace DTech.AdUnification
-{
-    internal sealed class AdResponse : IAdResponse
-    {
-        public AdType Type { get; set; }
-        public bool IsSuccessful { get; set; }
-    }
-}
